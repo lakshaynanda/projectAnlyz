3512d0deb169fa668bc81ab043643e9a94929409
lakshaynanda <lakshay.nanda08@gmail.com>
2020-07-23 03:16:52 +0530

    First Commit

---
+++ myapp/src/components/LoginForm.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class LoginForm extends React.Component {
+  state = {
+    username: '',
+    password: ''
+  };
+
+  handle_change = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState(prevstate => {
+      const newState = { ...prevstate };
+      newState[name] = value;
+      return newState;
+    });
+  };
+
+  render() {
+    return (
+      <div class="mainForm">
+        <form onSubmit={e => this.props.handle_login(e, this.state)}>
+            <h4 class="heading">Log In</h4>
+            <label htmlFor="username">Username</label>
+            <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            value={this.state.username}
+            onChange={this.handle_change}
+            />
+            <label htmlFor="password">Password</label>
+            <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handle_change}
+            />
+            <div class="wrapp">
+                <input type="submit" class="but"/>
+            </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
+
+LoginForm.propTypes = {
+  handle_login: PropTypes.func.isRequired
+};
---
+++ myapp/src/components/Nav.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Nav(props) {
+  const logged_out_nav = (
+    <ul>
+      <li class="item" onClick={() => props.display_form('login')}>Login</li>
+      <li class="item" onClick={() => props.display_form('signup')}>Signup</li>
+    </ul>
+  );
+
+  const logged_in_nav = (
+    <ul>
+      <li class="item" onClick={props.handle_logout}>Logout</li>
+    </ul>
+  );
+  return <div>{props.logged_in ? logged_in_nav : logged_out_nav}</div>;
+}
+
+export default Nav;
+
+Nav.propTypes = {
+  logged_in: PropTypes.bool.isRequired,
+  display_form: PropTypes.func.isRequired,
+  handle_logout: PropTypes.func.isRequired
+};
